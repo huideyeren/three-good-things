20df16f38beac1bdddfa872cda8b0f7d9cfc30d9
Takakura Yusuke <takakura.yusuke@gmail.com>
2018-02-12 14:38:19 +0900

    add firebase

---
+++ plugins/firebase.ts
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+require('dotenv').config();
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(
+    {
+      apiKey: process.env.APIKEY,
+      authDomain: process.env.AUTHDOMAIN,
+      databaseURL: process.env.DATABASEURL,
+      projectId: process.env.PROJECTID,
+      storageBucket: process.env.STORAGEBUCKET,
+      messagingSenderId: process.env.MESSAGINGSENDERID,
+    },
+  );
+}
+
+export default firebase;
